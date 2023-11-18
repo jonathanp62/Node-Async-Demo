be9f1cedb6e1ae31a2251eb92b73b68f13d1a190
Jonathan Parker <jonathanp62@gmail.com>
2023-11-18 15:40:17 -0500

    Completed the first phase

--- src/application.mjs
+++ src/application.mjs
@@ -7,6 +7,10 @@
  * All Rights Reserved.
  */
 
+import { ClassOne } from "./class-one.mjs";
+import { ClassTwo } from "./class-two.mjs";
+import { ClassThree } from "./class-three.mjs";
+
 /**
  * The application class.
  */
@@ -15,7 +19,26 @@ class Application {
      * The run method.
      */
     run() {
-	console.log('Hey!');
+	    const promise = new Promise(resolve => {
+            console.log('Beginning to run');
+            resolve(0);
+        });
+
+        const classOne = new ClassOne();
+        const classTwo = new ClassTwo();
+        const classThree = new ClassThree();
+
+        promise
+            .then(value => classOne.methodOne(value))
+            .then(value => classOne.methodTwo(value))
+            .then(value => classOne.methodThree(value))
+            .then(value => classTwo.methodOne(value))
+            .then(value => classTwo.methodTwo(value))
+            .then(value => classTwo.methodThree(value))
+            .then(value => classThree.methodOne(value))
+            .then(value => classThree.methodTwo(value))
+            .then(value => classThree.methodThree(value))
+            .then(value => console.log(`Ending the run with the last value: ${value}`));
     }
 }
 
